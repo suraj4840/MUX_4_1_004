68029a7a3cf9e61454162d122a9236afd68aa5d1
suraj4840 <144480393+suraj4840@users.noreply.github.com>
2023-09-23 23:40:21 +0530

    Create mux.v

---
+++ mux.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/07/2023 09:21:53 AM
+// Design Name: 
+// Module Name: jk_ff_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module mux_4_1(i0,i1,i2,i3,s0,s1,y);
+
+        input i0,i1,i2,i3;
+        input s0,s1;
+        output y;
+        
+        assign y = (~s1 & ~s0 & i0) | (~s1 & s0 & i1) | (s1 & ~s0 & i2) | (s1 & s0 & i3);
+        
+endmodule
